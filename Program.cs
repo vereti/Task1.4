7a16424eb28259e652d3622208edc936fabc846b
vereti <peace.atomka@yandex.ru>
2017-07-06 20:37:35 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, sub;
+            s = Console.ReadLine();
+            sub = Console.ReadLine();
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (sub.IndexOf(s[i]) == -1)
+                {
+                    Console.Write("нет ");
+                }
+                else
+                {
+                    Console.Write("да ");
+                }
+            }
+        }
+    }
+}
